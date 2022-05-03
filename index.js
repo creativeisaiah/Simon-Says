e75e05879bdd624dcbb7cc32477acdb20cb63671
Joshua Boyd <87743550+creativeisaiah@users.noreply.github.com>
2022-05-03 17:11:38 -0500

    timeout set for mobile

--- index.js
+++ index.js
@@ -107,7 +107,11 @@ function checkAnswer(currentLevel) {
     //3. Change the h1 title to say "Game Over, Press Any Key to Restart" if the user got the answer wrong.
     $("#level-title").text("Game Over, Press Any Key to Restart");
 
-    startOver();
+    // startOver();
+
+    setTimeout(function () {
+      startOver();
+    }, 200);
   }
 
 }
